bc6cfe7e2bb0660f680fb1f0dd1a6e1e93e5048d
Philip Münster-Hansen <phillermh@gmail.com>
2025-09-04 12:56:15 +0200

    finish injuries UI

--- src/app/medicin/injuries/page.tsx
+++ src/app/medicin/injuries/page.tsx
@@ -22,27 +22,27 @@ export default function InjuriesPage() {
     const iconColorMap: map = {
         'Alvorlig blødning': {
             icon: Heart,
-            numColor: 'bg-red-100',
+            numColor: 'bg-red-500',
             iconColor: 'bg-red-300'
         },
         'Forbrænding': {
             icon: Zap,
-            numColor: 'bg-yellow-100',
+            numColor: 'bg-yellow-500',
             iconColor: 'bg-yellow-300'
         },
         'Brud eller forstuvning': {
             icon: Bandage,
-            numColor: 'bg-blue-100',
+            numColor: 'bg-blue-500',
             iconColor: 'bg-blue-300'
         },
         'Forgiftning': {
             icon: TriangleAlert,
-            numColor: 'bg-orange-100',
+            numColor: 'bg-orange-500',
             iconColor: 'bg-orange-300'
         },
         'Bevidstløshed': {
             icon: Activity,
-            numColor: 'bg-green-100',
+            numColor: 'bg-green-500',
             iconColor: 'bg-green-300'
         }
     }
@@ -51,15 +51,27 @@ export default function InjuriesPage() {
         const Icon = iconColorMap[emergencyItem.type].icon
         const numColor = iconColorMap[emergencyItem.type].numColor
         const iconColor = iconColorMap[emergencyItem.type].iconColor
+        const labelColor = 
+            emergencyItem.label === 'KRITISK'
+            ? 'red'
+            : emergencyItem.label === 'VIGTIG'
+            ? 'gray'
+            : 'white'
 
         return (
             <div className="bg-neutral-50 border border-neutral-200 rounded-lg p-4 flex flex-col gap-4">
                 <div className="flex items-center gap-2">
                     <div className={clsx(iconColor, 'rounded-full', 'p-2')}><Icon className='h-4 w-4'/></div>
                     <p className="font-semibold">{emergencyItem.type}</p>
-                    <div className="ml-auto"><Badge color="gray">KRITISK</Badge></div>
-                    
+                    <div className="ml-auto"><Badge color={labelColor}>{emergencyItem.label}</Badge></div>
                 </div>
+                {emergencyItem.steps.map((step, index) => (
+                    <div key={index} className="flex items-center gap-4 p-3 rounded-lg bg-neutral-100 text-sm">
+                        <span className={clsx(numColor, "w-6 aspect-square rounded-full text-white font-semibold flex justify-center items-center text-sm shrink-0")}>{index + 1}</span>
+                        <p>{step}</p>
+                    </div>
+                ))}
+                
             </div>
         )
     })
